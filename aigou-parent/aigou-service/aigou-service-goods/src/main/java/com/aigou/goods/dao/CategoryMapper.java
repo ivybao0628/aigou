7c1014dc3dc0ea08ef3f8b6f61ae41e9384d4923
anchao <240853850@qq.com>
2020-02-20 17:12:47 +0800

    init

---
+++ aigou-parent/aigou-service/aigou-service-goods/src/main/java/com/aigou/goods/dao/CategoryMapper.java
@@ -0,0 +1,11 @@
+package com.aigou.goods.dao;
+import com.aigou.goods.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:shenkunlin
+ * @Description:Category的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface CategoryMapper extends Mapper<Category> {
+}
---
+++ aigou-parent/aigou-service/aigou-service-goods/src/main/java/com/aigou/goods/dao/StockBackMapper.java
@@ -0,0 +1,11 @@
+package com.aigou.goods.dao;
+import com.aigou.goods.pojo.StockBack;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:shenkunlin
+ * @Description:StockBack的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface StockBackMapper extends Mapper<StockBack> {
+}
